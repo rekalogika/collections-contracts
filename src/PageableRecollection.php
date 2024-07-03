359220f2b49efc5413061b5f2e65962ec8286031
root <1102197+priyadi@users.noreply.github.com>
2024-07-03 03:05:56 +0000

    refactor: add `PageableRecollection` (#91)

---
+++ src/PageableRecollection.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/collections package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Contracts\Collections;
+
+use Rekalogika\Contracts\Rekapager\PageableInterface;
+
+/**
+ * @template TKey of array-key
+ * @template-covariant T
+ * @extends PageableInterface<TKey,T>
+ */
+interface PageableRecollection extends PageableInterface, \Countable
+{
+    public function refreshCount(): void;
+}
